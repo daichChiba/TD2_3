4662221888b0e1a303417f72e24195a89fe88d2f
LC1D_17_Chiba_Daichi <146914435+daichChiba@users.noreply.github.com>
2024-12-04 20:21:45 +0900

    kamataEngineを導入し、GameSceneを導入したがDrawでエラーを吐く

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,6 +1,7 @@
 #include <KamataEngine.h>
 
 using namespace KamataEngine;
+#include "GameScene.h"
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -11,10 +12,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Audio* audio = nullptr;
 	AxisIndicator* axisIndicator = nullptr;
 	PrimitiveDrawer* primitiveDrawer = nullptr;
+	// ゲームシーンの作成
+	GameScene* gameScene = nullptr;
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+	win->CreateGameWindow(L"TD2_3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
@@ -51,6 +54,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	primitiveDrawer->Initialize();
 #pragma endregion
 
+	gameScene = new GameScene;
+	gameScene->Initialize();
+
 	// メインループ
 	while (true) {
 		// メッセージ処理
@@ -62,6 +68,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		imguiManager->Begin();
 		// 入力関連の毎フレーム処理
 		input->Update();
+		// ゲームシーンの作成
+		gameScene->Update();
 		// 軸表示の更新
 		axisIndicator->Update();
 		// ImGui受付終了
@@ -69,6 +77,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		// 描画開始
 		dxCommon->PreDraw();
+		// ゲームシーンの作成
+		gameScene->Draw();
 		// 軸表示の描画
 		axisIndicator->Draw();
 		// プリミティブ描画のリセット
