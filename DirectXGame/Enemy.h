81ca8dbaceb54cd6a239db141ec88bae05effdcc
LC1D_17_Chiba_Daichi <146914435+daichChiba@users.noreply.github.com>
2025-01-16 16:33:37 +0900

    治せているかテストしている。

--- DirectXGame/Enemy.h
+++ DirectXGame/Enemy.h
@@ -28,7 +28,7 @@ public:
 
 	void OnCollision();
 
-	// ???a??ï
+	// ???a??æ“¾
 	float GetRadius() { return radius_; }
 
 private:
@@ -43,7 +43,7 @@ private:
 
 	Player* player_;
 
-	// ”¼Œa
+	// åŠå¾„
 	float radius_ = 6.0f;
 };
 
