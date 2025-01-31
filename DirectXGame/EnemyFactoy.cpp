7d5f769d5c3961e44c67c4dc6e777306e7b01b79
wataru-hi <146914543+wataru-hi@users.noreply.github.com>
2025-01-31 10:09:14 +0900

    提出用に軽く修正

--- DirectXGame/EnemyFactoy.cpp
+++ DirectXGame/EnemyFactoy.cpp
@@ -10,7 +10,7 @@ void EnemyFactory::Initialize(Model* model, Model* bulletModel) {
 }
 
 std::unique_ptr<EnemyActor> EnemyFactory::AddEnemy() {
-	std::unique_ptr<EnemyActor> enemy_ = std::make_unique<EnamyBlackHole>();
+	std::unique_ptr<EnemyActor> enemy_ = std::make_unique<EnemyGravity>();
 	//enemy_->Initialize(model_, bulletModel_, Vector3{0.0f, 0.0f, 0.0f}, player_, gameScene_);
 	//enemy_->SetGameScene(gameScene_);
 	//enemy_->GetPlayer(player_);
--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"TD2_3");
+	win->CreateGameWindow(L"2367_アニマルファイターズ");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
